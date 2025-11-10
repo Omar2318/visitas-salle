23aa89248a6c9928f2c1b774084a61cfb02204ed
Omar <omar.ramirezm65@gmail.com>
2025-11-09 20:25:19 -0600

    Se añadio las cookies

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  
+  app.enableCors({
+    origin: process.env.FRONT_URL,
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   

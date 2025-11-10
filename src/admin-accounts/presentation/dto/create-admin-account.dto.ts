3ff7525658eefe1afb5ef59e7b7dfb8599a1e10c
Omar <omar.ramirezm65@gmail.com>
2025-11-09 20:12:19 -0600

    Documentación basica de admin-accounts

--- src/admin-accounts/presentation/dto/create-admin-account.dto.ts
+++ src/admin-accounts/presentation/dto/create-admin-account.dto.ts
@@ -2,12 +2,15 @@ import { CreateAdminAccountInput } from "src/admin-accounts/application/inputs";
 import { UniversityRole } from "src/admin-accounts/domain/enums";
 import { CreateUserDto } from '../../../common/dto/create-user.dto';
 import { IsEnum, IsString, IsUUID, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateAdminAccountDto extends CreateUserDto implements CreateAdminAccountInput{
     @IsEnum(UniversityRole)
+    @ApiProperty({enum: UniversityRole})
     role: UniversityRole;
 
     @IsUUID()
     @MinLength(3)
+    @ApiProperty({})
     areaId: string;
 }
